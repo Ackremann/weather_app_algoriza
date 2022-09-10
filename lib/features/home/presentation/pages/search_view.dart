d1bcac98d93d7e9177f80e190cecb44400814b10
Mahmoud Sherif <m1shery2@gmail.com>
2022-09-10 07:32:28 +0200

    last config

--- lib/features/home/presentation/pages/search_view.dart
+++ lib/features/home/presentation/pages/search_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/core/router/router.dart';
@@ -12,53 +13,60 @@ class SearchView extends StatelessWidget {
     return BlocProvider(
       create: (context) => HomeCubit(),
       child: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Builder(
-                builder: (context) {
-                  final cubit = HomeCubit.of(context);
-                  return TextField(
-                    onSubmitted: (value) async {
-                      await cubit.search(value);
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  MagicRouter.navigateAndPopAll(HomeView());
+                },
+                icon: Icon(Icons.home))
+          ],
+          title: Builder(
+            builder: (context) {
+              final cubit = HomeCubit.of(context);
+              return TextField(
+                onSubmitted: (value) async {
+                  await cubit.search(value);
 
-                      MagicRouter.navigateAndPopAll(
-                        HomeView(
-                          path:
-                              'q=${cubit.weather!.location!.lat},${cubit.weather!.location!.lon}',
-                        ),
-                      );
-                    },
+                  MagicRouter.navigateAndPopAll(
+                    HomeView(
+                      path:
+                          'q=${cubit.weather!.location!.lat},${cubit.weather!.location!.lon}',
+                    ),
                   );
                 },
-              ),
-              BlocBuilder<HomeCubit, HomeState>(
-                builder: (context, state) {
-                  final cubit = HomeCubit.of(context);
-                  if (state is SearchLodaing) {
-                    const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (cubit.weather == null) {
-                    return const Center(
-                      child: Text('Invalid City name'),
-                    );
-                  } else {
-                    final weather = cubit.weather;
-                    return Column(
-                      children: [
-                        ListTile(
-                          title: Text(weather!.location!.name!),
-                          subtitle: Text(weather.location!.country!),
-                        )
-                      ],
-                    );
-                  }
-                  return Text('Invalid City name');
-                },
-              ),
-            ],
+              );
+            },
           ),
+        ),
+        body: Column(
+          children: [
+            BlocBuilder<HomeCubit, HomeState>(
+              builder: (context, state) {
+                final cubit = HomeCubit.of(context);
+                if (state is SearchLodaing) {
+                  const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (cubit.weather == null) {
+                  return const Center(
+                    child: Text('Invalid City name'),
+                  );
+                } else {
+                  final weather = cubit.weather;
+                  return Column(
+                    children: [
+                      ListTile(
+                        title: Text(weather!.location!.name!),
+                        subtitle: Text(weather.location!.country!),
+                      )
+                    ],
+                  );
+                }
+                return Center(child: Text('Invalid City name'));
+              },
+            ),
+          ],
         ),
       ),
     );
